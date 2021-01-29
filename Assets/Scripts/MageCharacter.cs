fc3f991d784cd51b19793366054ac7133c670200
Kevin Grandez <kevinlg08@gmail.com>
2021-01-29 11:23:24 -0300

    Import Animation Sprites / CharacterMovement

---
+++ Assets/Scripts/MageCharacter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MageCharacter : MonoBehaviour
+{
+    float moveX;
+    float moveY;
+     
+    public float movementSpeed; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetPlayerInput();
+        MovePlayer();
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    private void GetPlayerInput()
+    {
+        moveX = Input.GetAxisRaw("Horizontal");
+        moveY = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        Vector3 directionVector = new Vector3(moveX,moveY, 0);
+        transform.Translate(directionVector.normalized * Time.deltaTime * movementSpeed);
+    }
+}
